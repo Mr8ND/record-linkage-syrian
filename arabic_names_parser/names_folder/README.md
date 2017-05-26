d6d3c06cc1ebced8c9e6f694b31773a19d30b22c
ndalmass <ndalmass@cmu.andrew.edu>
2017-05-26 18:46:12 -0400

    Committing a name folder, in which names or part of names of which we know the exact name part are going to be stored

---
+++ arabic_names_parser/names_folder/README.md
@@ -0,0 +1,2 @@
+This folder contains specific names that are either laqab or nisbah for both man or women.
+The laqab and nisbah were taken directly from http://heraldry.sca.org/names/arabic-naming2.htm#foot2.
---
+++ arabic_names_parser/names_folder/specific_names.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+def createVectorNamesFromFile(filename):
+	'''
+	This function, given a specific file path, given it is a txt, will extract every rows and will create a list in which each element is a row without the flags \n
+
+	INPUT
+	- filename: that is just the filepath, it is expected to be a txt
+
+	OUTPUT
+	- output_vec: a list, in which each element is a line of the file without new line flags \n
+	'''
+
+	output_vec = []
+	with open(filename) as f:
+		lines = f.readlines()
+		for line in lines:
+			output_vec.append(line.replace('\n', ''))
+
+	return output_vec
+
+
+if __name__ == '__main__':
+	print createVectorNamesFromFile('laqab_M.txt')
