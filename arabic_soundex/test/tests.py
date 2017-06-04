00b6bd504548916c78adb0898894ead57a269d57
ndalmass <ndalmass@cmu.andrew.edu>
2017-06-04 18:25:45 -0400

    Tests changed accordingly to the change in the arabic soundex

--- arabic_soundex/test/tests.py
+++ arabic_soundex/test/tests.py
@@ -4,29 +4,30 @@ import unittest2 as unittest
 import sys
 sys.path.append("..")
 
-from arabic_soundex import arabic_soundex_main
+from arabic_soundex import arabic_soundex_names
+
 
 class MyTest(unittest.TestCase):
 
 	#Test0 makes sure that the function throws the correct expection when an empty string is given
 	def test0(self):
-		self.assertRaises(TypeError, arabic_soundex_main, '')
+		self.assertRaises(TypeError, arabic_soundex_names, '')
 
 
 	#This first test will test non-arabic characters string
 	def test1(self):
-		self.assertEqual(arabic_soundex_main('ciao'), 'xiao')
-		self.assertEqual(arabic_soundex_main('ciao!!!'), 'xiao!')
-		self.assertEqual(arabic_soundex_main('_~|'), 'x~|')
+		self.assertEqual(arabic_soundex_names('ciao'), u'xiao')
+		self.assertEqual(arabic_soundex_names('ciao!!!'), u'xiao')
+		self.assertEqual(arabic_soundex_names('_~|'), u'x~|0')
 
 
 	#Second test will work on non-arabic characters string with the the two toggles
 	#in all the possible combinations
 	def test2(self):
-		self.assertEqual(arabic_soundex_main('ciao', firstcharuniforming=False), 'ciao')
+		self.assertEqual(arabic_soundex_names('ciao', firstcharuniforming=False), u'ciao')
 
 	def test3(self):
-		self.assertEqual(arabic_soundex_main(unicode('ابب', 'utf-8')), 'x1')
+		self.assertEqual(arabic_soundex_names(unicode('ابب', 'utf-8')), u'x100')
 
 #	def test4(self):
 #		self.assertEqual(arabic_soundex_main('ابب'), 'x1')
