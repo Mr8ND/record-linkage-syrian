f2b5ed7ddfa4d5d20c01b7c5e72461a17562ec19
ndalmass <ndalmass@cmu.andrew.edu>
2017-07-02 10:53:25 -0400

    Committing work on general features

--- features_file.py
+++ features_file.py
@@ -249,15 +249,15 @@ def samePrefixWordsFunction(s1, s2, n=3):
 	s1_w, s2_w = s1.split(' '), s2.split(' ')
 	s_short, s_long = obtainLongerShorterFunction(s1_w, s2_w)
 	
-	same_suff = []
+	same_pre = []
 	for ss in s_short:
-		same_suff.append(sum([int(ss[:n]==sl[:n]) for sl in s_long])>=1)
-	return all(same_suff)
+		same_pre.append(sum([int(ss[:n]==sl[:n]) for sl in s_long])>=1)
+	return int(all(same_pre))
 
 
 def sameSuffixWordsFunction(s1, s2, n=3):
 	'''
-	This function returns whether in the last n characters of each word in the shorter string are present in one of the words of the longer strings. Removing some edge cases which are not considered problematic, the function checks whether the suffixes of the words of the two strings are the same. n stands for the number of characters of each prefix (custom).
+	This function returns whether in the last n characters of each word in the shorter string are present in one of the words of the longer strings. Removing some edge cases which are not considered problematic, the function checks whether the suffixes of the words of the two strings are the same. n stands for the number of characters of each suffix (custom).
 
 	INPUT:
 	- s1: first string
@@ -273,7 +273,209 @@ def sameSuffixWordsFunction(s1, s2, n=3):
 	same_suff = []
 	for ss in s_short:
 		same_suff.append(sum([int(ss[::-1][:n]==sl[::-1][:n]) for sl in s_long])>=1)
-	return all(same_suff)
+	return int(all(same_suff))
+
+
+def numberSameCharFunction(s1, s2, last=False):
+	'''
+	This function returns the number of first or last n characters that are exactly the same. The counter will stop as soon as one character is different.
+
+	INPUT:
+	- s1: the first string
+	- s2: the second string
+	- last: logical flag, if True the last n characters are considered
+
+	OUTPUT:
+	Number of n first/last characters which are the same.
+	'''
+
+	s_short, s_long = obtainLongerShorterFunction(s1, s2)
+	if last:
+		s_short, s_long = s_short[::-1], s_long[::-1]
+
+	counter, flag, limit = 0, True, len(s_short)
+	while flag and counter < limit:
+		if s_short[counter] == s_long[counter]:
+			counter +=1
+		else:
+			flag = False
+
+	return counter
+
+
+def numberSameWordFunction(s1, s2, last=False):
+	'''
+	This function returns the number of first or last n words that are exactly the same. The counter will stop as soon as one word is different.
+
+	INPUT:
+	- s1: the first string
+	- s2: the second string
+	- last: logical flag, if True the last n words are considered
+
+	OUTPUT:
+	Number of n first/last words which are the same.
+	'''
+	s1_w, s2_w = s1.split(' '), s2.split(' ')
+	s_short, s_long = obtainLongerShorterFunction(s1_w, s2_w)
+	if last:
+		s_short, s_long = s_short[::-1], s_long[::-1]
+
+	counter, flag, limit = 0, True, len(s_short)
+	while flag and counter < limit:
+		if s_short[counter] == s_long[counter]:
+			counter +=1
+		else:
+			flag = False
+
+	return counter
+
+
+def alignedCharFunction(s1, s2, perc=True):
+	'''
+	This function returns the percentage of aligned characters, intended as the ratio between the number of aligned characters between the shorter string and the longer string over the length of the shorter string. It can also return the number of aligned characters.
+
+	INPUT:
+	- s1: the first string
+	- s2: the second string
+	- perc: logical flag. If True, the percentage is returned, if False the absolute number.
+
+	OUTPUT:
+	percentage if perc==True else the absolute number
+	'''
+
+	s_short, s_long = obtainLongerShorterFunction(s1, s2)
+	al_vec = [int(s_short[j]==s_long[j]) for j in range(len(s_short))]
+	if perc:
+		return float(sum(al_vec))/len(al_vec)
+	else:
+		return sum(al_vec)
+
+
+def alignedWordFunction(s1,s2, perc=True):
+	'''
+	This function returns the percentage of "aligned" words (which are the words in the same position) intended as the ratio between the number of aligned words between the shorter string and the longer string over the length of the shorter string. It can also return the number of aligned words.
+
+	INPUT:
+	- s1: the first string
+	- s2: the second string
+	- perc: logical flag. If True, the percentage is returned, if False the absolute number.
+
+	OUTPUT:
+	percentage if perc==True else the absolute number
+	'''
+
+	s1_w, s2_w = s1.split(' '), s2.split(' ')
+	s_short, s_long = obtainLongerShorterFunction(s1_w, s2_w)
+
+	al_vec = [int(s_short[j]==s_long[j]) for j in range(len(s_short))]
+	if perc:
+		return float(sum(al_vec))/len(al_vec)
+	else:
+		return sum(al_vec)
+
+
+def effectiveWordDiffFunction(s1, s2):
+	'''
+	This function finds the number of words that are different between to strings, ideally composed of multiple words. The idea is that this function accounts for shuffled positions and also considers the shorter string as reference point. If all the words of the shorter string are included in the longer string, than there is effectively no difference between the two strings as one is contained into the other.
+
+	INPUT:
+	- s1: the first string
+	- s2: the second string
+
+	OUTPUT:
+	- sshort_notfound: the words in the shorter strings that are not found in the longer string
+	- s_long: the words in the longer string that have not been paired with words in the shorter string
+	'''
+
+	s1_w, s2_w = s1.split(' '), s2.split(' ')
+	s_short, s_long = obtainLongerShorterFunction(s1_w, s2_w)
+	
+	inc, sshort_notfound = 0, []
+	while inc<len(s_short):
+		word_sel = s_short[inc]
+		try:
+			idx_2 = s_long.index(word_sel)
+			del s_long[idx_2]
+			inc +=1
+		except ValueError:
+			sshort_notfound.append(word_sel)
+			inc +=1
+	return sshort_notfound, s_long
+
+
+def nWordDiffFunction(s1, s2, perc=True):
+	'''
+	This function is just a wrapper of effectiveWordDiffFunction above, returning the number/percentage of differing words between the shorter string and the longer string.
+
+	INPUT:
+	- s1: the first string
+	- s2: the second string
+	- perc: logical flag, if True the percentage is returned, else the absolute number
+
+	OUTPUT:
+	percentage of differing words if perc==True, else the absolute number 
+	'''
+	sshort_notfound, s_long = effectiveWordDiffFunction(s1,s2)
+	len_short = min([len(s1.split(' '), s2.split(' '))])
+
+	if perc:
+		return float(len(sshort_notfound))/len_short
+	else:
+		return len(sshort_notfound)
+
+
+def nSameSuffixFunction(s1, s2, n=3, perc=True):
+	'''
+	This function returns the number/percentage of words agreeing in the last n characters between the shorter and longer strings. Removing some edge cases which are not considered problematic, the function checks the number/percentage of suffixes of the words of the two strings are the same. n stands for the number of characters of each suffix (custom).
+
+	INPUT:
+	- s1: first string
+	- s2: second string
+	- n: number of characters of each suffix
+	- perc: logical flag, if True the percentage is returned, otherwise the absolute number is returned
+
+	OUTPUT:
+	percentage if perc==True else the absolute number
+	'''
+
+	s1_w, s2_w = s1.split(' '), s2.split(' ')
+	s_short, s_long = obtainLongerShorterFunction(s1_w, s2_w)
+	
+	same_suff = []
+	for ss in s_short:
+		same_suff.append(sum([int(ss[::-1][:n]==sl[::-1][:n]) for sl in s_long])>=1)
+	
+	if perc:
+		return float(sum(same_suff))/len(s_short)
+	else:
+		return sum(same_suff)
+
+
+def nSamePrefixFunction(s1, s2, n=3, perc=True):
+	'''
+	This function returns the number/percentage of words agreeing in the first n characters between the shorter and longer strings. Removing some edge cases which are not considered problematic, the function checks the number/percentage of prefixes of the words of the two strings are the same. n stands for the number of characters of each prefix (custom).
+
+	INPUT:
+	- s1: first string
+	- s2: second string
+	- n: number of characters of each prefix
+	- perc: logical flag, if True the percentage is returned, otherwise the absolute number is returned
+
+	OUTPUT:
+	percentage if perc==True else the absolute number
+	'''
+
+	s1_w, s2_w = s1.split(' '), s2.split(' ')
+	s_short, s_long = obtainLongerShorterFunction(s1_w, s2_w)
+	
+	same_pre = []
+	for ss in s_short:
+		same_pre.append(sum([int(ss[:n]==sl[:n]) for sl in s_long])>=1)
+	
+	if perc:
+		return float(sum(same_pre))/len(s_short)
+	else:
+		return sum(same_pre)
 
 
 feature_dict = {
@@ -303,6 +505,19 @@ feature_dict = {
 		'same_sorted_string': sortedArabicFunction,
 		'same_prefixes': samePrefixWordsFunction,
 		'same_suffixes': sameSuffixWordsFunction
+	},
+
+	'general_features':{
+		'n_chars_first': numberSameCharFunction,
+		'n_chars_last': partial(numberSameCharFunction, last=True),
+		'n_words_first': numberSameWordFunction,
+		'n_words_last': partial(numberSameWordFunction, last=True),
+		'align_words_perc': alignedWordFunction,
+		'align_char_perc': alignedCharFunction,
+		'same_sorted_string': sortedArabicFunction,
+		'perc_agreeing_words': nWordDiffFunction,
+		'perc_same_suffixes': nSameSuffixFunction,
+		'perc_same_prefixes': nSamePrefixFunction
 	}
 }
 
